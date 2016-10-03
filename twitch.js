4825b4c60e5a990840ee3807160d756d71581c21
Lidiya <lemi.dev@gmail.com>
2016-10-03 10:26:36 +0100

    Comment out JS file

--- twitch.js
+++ twitch.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
+  //array of streamers the Twitch Tool will display info for
   var streamers = ['freecodecamp', 'syndicate', 'riotgames', 'esl csgo', 'Nightblue3', 'summit1g', 'LIRIK', 'PhantomL0rd', 'imaqtpie', 'captainsparklez', 'sodapoppin', 'goldglove', 'tsm bjergsen', 'Joshdog', 'Tsm dyrus', 'mushisgoshu', 'trick2g', 'comster404', 'brunofin'];
   var status, url, picture, x = 0;
-
+    
+    
+  /*********************
+  * Append the rows
+  **********************/
   function updateHTML(section) {
     $(section).append('<div class="row streamer"><div class="proflogo" id="user-image-' + x + '"></div><span class="status">' + status + '</span></div></div>');
     if (section == ".online" || section == ".offline") {
@@ -13,6 +18,10 @@ $(document).ready(function() {
     x++;
   }
 
+    
+  /************************************
+  * Show online, offline and all users
+  *************************************/
   function showOnline() { //Show only online users
     $(".offline-users, .all-users").removeClass('focus');
     $(".online-users").addClass('focus');
@@ -34,8 +43,9 @@ $(document).ready(function() {
   }
 
 
-
-  //fetch the data for each streamer using ajax requests
+  /*****************************************************
+  *fetch the data for each streamer using ajax requests
+  ******************************************************/
   for (var i = 0; i < streamers.length; i++) {
     ajax();
   }
@@ -53,7 +63,7 @@ $(document).ready(function() {
       },
 
       error: function() {
-        console.log("unable to access json");
+        console.log("Unable to access json");
       }
     })
   }
@@ -61,23 +71,28 @@ $(document).ready(function() {
   function fetchData(data) {
     if (data.stream === null) {
       url = data._links.channel.substr(38);
-      updateOfflineUsers();
+      updateOfflineUsers(); //offline userts
+        
     } else if (data.status == 422 || data.status == 404) {
       status = data.message;
-      updateHTML(".unavailable");
+      updateHTML(".unavailable"); //unavaiable userts
+        
     } else {
       if (data.stream.channel.logo !== null) {
-        picture = 'url("' + data.stream.channel.logo + '")';
+        picture = 'url("' + data.stream.channel.logo + '")'; //no logo, so insert general logo
       } else {
         picture = 'url("http://seeklogo.com/images/T/twitch-logo-4931D91F85-seeklogo.com.png")';
       }
+        
       url = data._links.channel.substr(38);
       status = "<a href='https://twitch.tv/" + url + "' target='_blank'" + "'>" + data.stream.channel.display_name + "</a>" + " is currently streaming " + data.stream.game;
-      updateHTML(".online");
+      updateHTML(".online"); //online users
     }
   }
 
-  //another API call for more info on the offline users
+  /*****************************************************
+  * Another API call for more info on the offline users
+  ******************************************************/
   function updateOfflineUsers() {
     $.ajax({
       url: "https://api.twitch.tv/kraken/channels/" + url + "?client_id=hpwim380yptokdwj4n6xi9dgjr82w3u",
@@ -96,11 +111,14 @@ $(document).ready(function() {
       }
     });
   }
-
+    
+  /*******************************************************
+  * Add functionality to the buttons displaying the users
+  ********************************************************/
   $('#all button').on('click', function() {
     showAll();
   });
-
+    
   $('#online button').on('click', function() {
     showOnline();
   });
